ad522d0048d90f6eb57ef6a8bd28a927fa3f32cd
Gustav Lindqvist <hej@gustavlindqvist.se>
2023-01-23 19:15:25 +0100

    more fixes…don't code when tired

--- CreateMap.sh
+++ CreateMap.sh
@@ -8,18 +8,20 @@ STYLEFILE=$PWD"/style/opentopomap"
 TYPFILETXT=$PWD"/style/typ/opentopomap.txt"
 TYPFILE=$PWD"/style/typ/opentopomap.typ"
 DATA=$PWD"/images/6324*.pbf"
+SOURCE=$PWD"/sweden-latest.osm.pbf"
 
 rm -rf "images"
 mkdir images
 cd images
-java -jar $SPLITTERJAR --precomp-sea=$SEA "../sweden-latest.osm.pbf"
-cd ..
+java -jar $SPLITTERJAR --precomp-sea=$SEA $SOURCE
 java -cp $MKGMAPJAR uk.me.parabola.mkgmap.main.TypCompiler $TYPFILETXT $TYPFILE
+cd ..
 echo 'Compiled TYPFILE'
 rm -rf "output/*"  || true
 java -jar $MKGMAPJAR -c $OPTIONS --style-file=$STYLEFILE --family-name="Reedyn OSM Map ($(date +"%Y-%m-%d %H:%I"))" --precomp-sea=$SEA --output-dir=output --bounds=$BOUNDS $DATA $TYPFILE
 echo "Copying file to root"
 sleep 1
+echo $PWD
 cp "output/gmapsupp.img" "OSM_Sweden_Reedyn.img"
 echo "Done"
 sleep 10
